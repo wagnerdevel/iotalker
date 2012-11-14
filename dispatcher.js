69490955d2d1bca593a60668e13fae3723d0ca3f
Wagner Silveira <wagnerdevel@gmail.com>
2012-11-14 01:16:40 -0200

    Removido problema de processos recursivos em caso de problemas em subscribers.

--- dispatcher.js
+++ dispatcher.js
@@ -8,88 +8,79 @@ var config = require('./config').dispatcher();
 var Subscriber = require('./models/subscriber.js').make(Schema, mongoose);
 var Queue = require('./models/queue.js').make(Schema, mongoose);
 
-/**
- * Envia os dados de um publicante para um assinante
- *
- * @param String domain [description]
- * @param Integer port Número da porta
- * @param String path [description]
- * @param String method HTTP Method (POST, GET, PUT...)
- * @param Object data Objeto JSON. Pode ser vazio.
- * @return void [description]
- */
-function dispatcherRest(domain, port, path, method, data) {
-	data = querystring.stringify(data);
+function SimpleQueue() {
+	var queue = [];
+	
+	this.enqueue = function(object) {
+		queue.push(object);
+	},
+	
+	this.dequeue = function() {
+		queue.shift();
+	},
+	
+	this.length = function() {
+		return queue.length;
+	},
+	
+	this.getQueue = function() {
+		return queue;
+	};
+}
+
+function dispatcherRest(queue, subscriber, index) {
+	var data = querystring.stringify(queue.data);
 	
 	var options = {
-		host: domain,
-		port: port,
-		path: path,
-		method: method,
+		host: subscriber.domain,
+		port: subscriber.port,
+		path: subscriber.path,
+		method: subscriber.method,
 		agent: http.globalAgent,
 		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-			'Content-Length': data.length
+			'content-type': 'application/x-www-form-urlencoded',
+			'content-length': data.length
 		}
 	};
 	
 	var request = http.request(options, function(response) {
 		response.setEncoding('utf8');
 		
-		response.on('data', function (chunk) {
-			console.log('Response: ' + chunk);
+		response.on('data', function () {
+			console.log('from publisher_id: '+ queue.publisher_id +' to subscriber_id: '+ subscriber._id);
 		});
+	});
+	
+	request.on('error', function (e) {
+		console.log('erro ao enviar dados');
+		console.log(e);
 	});
 	
 	if (data.length > 0)
 		request.write(data);
 	
 	request.end();
+}
+
+function saveEventError(event) {
 	
-	return true;
 }
 
 function dispatcher() {
-	Queue.find({}, function (err, queues) {
+	Queue.find({}, null, {sort: {datetime: 1}}, function (err, queues) {
 		if (! err && queues.length > 0) {
 			queues.forEach(function (queue) {
-				var deleted = false;
-				
-				queue.subscribers.forEach(function (subscriber, index) {
-					var result = dispatcherRest(
-						subscriber.domain,
-						subscriber.port,
-						subscriber.path,
-						subscriber.method,
-						queue.data
-					);
-					
-					if (result) {
-						queue.subscribers.splice(index, 1);
+				Queue.findByIdAndRemove(queue._id, function (err) {
+					if (err) {
+						console.log('Erro ao remover evento do BD. Recursão prevista. Detalhes:');
 						
-						deleted = true;
-						
-						console.log('from publisher_id: '+ queue.publisher_id +' to subscriber_id: '+ subscriber._id);
+						console.log(err);
 					}
 				});
 				
-				if (queue.subscribers.length > 0 && deleted) {
-					var upsertData = queue.toObject();
-					
-					delete upsertData._id;
-					
-					Queue.update({_id: queue.id}, upsertData, {upsert: true}, function (error) {
-						if (error) {
-							console.log('Um erro ocorreu: '+ error);
-						}
-					});
-				} else if (deleted) {
-					Queue.findByIdAndRemove(queue.id, function (err) {
-						if (err) {
-							console.log('Um erro ocorreu para remover a queue. Possível recursão ocorrerá.');
-						}
-					});
-				}
+				queue.subscribers.forEach(function (subscriber, index) {
+					dispatcherRest(queue, subscriber, index);
+				});
 			});
 		}
 	});
